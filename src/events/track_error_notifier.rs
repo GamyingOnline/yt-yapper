0159cefc3ed0931ac1cd2f687b02fe96eadbe104
ayamdobhal <me@iamdobhal.dev>
2025-01-22 20:08:52 +0530

    fix(track_error_notifier): pop track on error and send message

--- src/events/track_error_notifier.rs
+++ src/events/track_error_notifier.rs
@@ -1,7 +1,7 @@
 use tokio::sync::mpsc::Sender;
 
 use ::serenity::async_trait;
-use serenity::all::GuildId;
+use serenity::all::{Colour, CreateEmbed, CreateMessage, GuildId};
 use songbird::{
     events::{Event, EventContext, EventHandler as VoiceEventHandler},
     tracks::PlayMode,
@@ -38,18 +38,53 @@ impl VoiceEventHandler for TrackErrorNotifier {
                 .unwrap();
             if let Ok(Some(track)) = response.await {
                 if track.handle_uuid == handle.uuid().to_string() {
-                    if state.playing == PlayMode::End || state.playing == PlayMode::Stop {
-                        let (responder, response) =
-                            tokio::sync::oneshot::channel::<Option<Track>>();
-                        self.queues
-                            .send(QueueMessage::Pop {
-                                key,
-                                responder,
-                                event_state: self.event_state.clone(),
-                            })
-                            .await
-                            .unwrap();
-                        let _ = response.await;
+                    match &state.playing {
+                        PlayMode::Play => {}
+                        PlayMode::Pause => {}
+                        PlayMode::Errored(_) => {
+                            let (responder, response) =
+                                tokio::sync::oneshot::channel::<Option<Track>>();
+                            self.queues
+                                .send(QueueMessage::Pop {
+                                    key,
+                                    responder,
+                                    event_state: self.event_state.clone(),
+                                })
+                                .await
+                                .unwrap();
+                            let _ = response.await;
+                            let error_embed = CreateEmbed::new()
+                                .title("❌ **Error while Playing Track**")
+                                .description(format!("{} - {}", track.artist, track.name))
+                                .field(
+                                    "An unexpected error is stopping me from playing this track.",
+                                    "This is most probably due to YouTube API.",
+                                    false,
+                                )
+                                .thumbnail(track.thumbnail)
+                                .color(Colour::from_rgb(255, 0, 0));
+                            self.event_state
+                                .text_channel_id
+                                .send_message(
+                                    &self.event_state.context,
+                                    CreateMessage::new().add_embed(error_embed),
+                                )
+                                .await
+                                .unwrap();
+                        }
+                        _ => {
+                            let (responder, response) =
+                                tokio::sync::oneshot::channel::<Option<Track>>();
+                            self.queues
+                                .send(QueueMessage::Pop {
+                                    key,
+                                    responder,
+                                    event_state: self.event_state.clone(),
+                                })
+                                .await
+                                .unwrap();
+                            let _ = response.await;
+                        }
                     }
                 }
             }
